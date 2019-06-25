5ffb083d9cd4f2607d4c9d09a5754f6b14523f1e
Jared <jared@needapart.co.nz>
2019-06-25 21:20:53 +1200

    Play around with UI Grid, Buttons and Header

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import {Grid, Button, Container } from 'semantic-ui-react'
+import HeaderBar from './components/HeaderBar'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Container>
+        <Grid columns={5}>
+          <HeaderBar />
+          <Grid.Row>
+            <Grid.Column>
+              <Button fluid>Number 1</Button>
+            </Grid.Column>
+            <Grid.Column>
+              <Button fluid>Number 2</Button>
+            </Grid.Column>
+            <Grid.Column>
+              <Button fluid>Number 3</Button>
+            </Grid.Column>
+            <Grid.Column>
+              <Button fluid>Number 4</Button>
+            </Grid.Column>
+            <Grid.Column>
+              <Button fluid>Number 5</Button>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Container>
+    </React.Fragment>
   );
 }
 
-export default App;
+export default App
---
+++ src/components/HeaderBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Grid, Header } from 'semantic-ui-react'
+
+const HeaderBar = () => {
+  return (
+    <Grid.Row>
+      <Header as='h1'>Semantic UI</Header>
+    </Grid.Row>
+  )
+}
+
+export default HeaderBar
